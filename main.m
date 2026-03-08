9a3de5531d0a423ee4c96c257d57547e07f6a55c
dj-viking <anders.swedishviking@gmail.com>
2026-03-07 19:35:53 -0500

    update

--- main.m
+++ main.m
@@ -6,18 +6,26 @@
 //
 #import <Cocoa/Cocoa.h>
 int main ()
-    {
-        @autoreleasepool {
-            [NSApplication sharedApplication];
-            [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-            id applicationName = [[NSProcessInfo processInfo] processName];
-            id window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 800, 600)
-                styleMask:NSWindowStyleMaskTitled  backing:NSBackingStoreBuffered defer:NO];
-            [window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
-            [window setTitle: applicationName];
-            [window makeKeyAndOrderFront:nil];
-            [NSApp activateIgnoringOtherApps:YES];
-            [NSApp run];
-        }
-        return 0;
+{
+	@autoreleasepool {
+		//# init app
+		[NSApplication sharedApplication];
+		[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+		id applicationName = [[NSProcessInfo processInfo] processName];
+	
+		//# create window
+		id window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 800, 600)
+					                styleMask:NSWindowStyleMaskTitled  
+					                  backing:NSBackingStoreBuffered 
+	 				                    defer:NO
+			    ];
+		[window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
+		[window                setTitle:applicationName];
+		[window    makeKeyAndOrderFront:nil];
+
+		//# start app (blocks thread)
+		[NSApp activateIgnoringOtherApps:YES];
+		[NSApp run];
+	}
+	return 0;
 } 
